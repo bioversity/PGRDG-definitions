eca1cda41d27fbebf8e0ea1ea1d9e0a11538c88c
Milko Škofič <info@skofic.net>
2015-03-10 12:16:05 +0100

    Added new API operations. Added new result formatting flags.

--- Api.inc.php
+++ Api.inc.php
@@ -1626,6 +1626,121 @@ define( "kAPI_OP_GET_MANAGED",					'getManaged' );
  */
 define( "kAPI_OP_CHECK_USER_CODE",				'checkUserCode' );
 
+/**
+ * Upload template.
+ *
+ * This tag defines the upload template operation.
+ *
+ * This operation expects the following parameters:
+ *
+ * <ul>
+ *	<li><tt>{@link kAPI_REQUEST_LANGUAGE}</tt>: <em>Language</em>. If the parameter is
+ *		omitted, the {@link kSTANDARDS_LANGUAGE} constant will be used. The value represents
+ *		the default language code.
+ *	<li><tt>{@link kAPI_REQUEST_USER}</tt>: <em>User</em>. The identifier,
+ *		{@link kTAG_IDENTIFIER}, of the user sending the template, or the user
+ *		code/password combination as an array. If the user is not allowed to submit
+ *		templates, the service will return an error.
+ *	<li><tt>{@link kAPI_PARAM_FILE_PATH}</tt>: <em>File path</em>. This required parameter
+ *		holds the template file path.
+ * </ul>
+ *
+ * The service will return the identifier of the template upload session.
+ */
+define( "kAPI_OP_UPLOAD_TEMPLATE",				'uploadTemplate' );
+
+/**
+ * Check session progress.
+ *
+ * This tag defines the check session progress operation.
+ *
+ * This operation expects the following parameters:
+ *
+ * <ul>
+ *	<li><tt>{@link kAPI_REQUEST_LANGUAGE}</tt>: <em>Language</em>. If the parameter is
+ *		omitted, the {@link kSTANDARDS_LANGUAGE} constant will be used. The value represents
+ *		the default language code.
+ *	<li><tt>{@link kAPI_REQUEST_USER}</tt>: <em>User</em>. The identifier,
+ *		{@link kTAG_IDENTIFIER}, of the user requesting the session information, or the user
+ *		code/password combination as an array. If the user is not allowed to submit
+ *		templates, the service will return an error.
+ *	<li><tt>{@link kAPI_PARAM_ID}</tt>: <em>Session ID</em>. This required parameter
+ *		holds the session identifier we are probing.
+ * </ul>
+ *
+ * The service will return the session and root level transactions information.
+ */
+define( "kAPI_OP_SESSION_PROGRESS",				'getSessionProgress' );
+
+/**
+ * Put data.
+ *
+ * This tag defines the put data operation.
+ *
+ * This service will store the data provided in the {@link kAPI_PARAM_OBJECT} parameter
+ * into the <tt>{@link kSTANDARDS_PORTAL_COLLECTION}</tt> collection in the users database.
+ *
+ * This operation expects the following parameters:
+ *
+ * <ul>
+ *	<li><tt>{@link kAPI_REQUEST_USER}</tt>: <em>User</em>. The identifier,
+ *		{@link kTAG_IDENTIFIER}, of the user requesting the operation, or the user
+ *		code/password combination as an array. If the user does not have the role
+ *		{@link kTYPE_ROLE_EDIT}, the service will return an error.
+ *	<li><tt>{@link kAPI_PARAM_OBJECT}</tt>: <em>Object</em>. The data to be stored as a
+ *		full json record.
+ * </ul>
+ *
+ * The service will return the stored data identifier.
+ */
+define( "kAPI_OP_PUT_DATA",						'putData' );
+
+/**
+ * Get data.
+ *
+ * This tag defines the get data operation.
+ *
+ * This service will retrieve the data identified by the {@link kAPI_PARAM_ID} parameter
+ * from the <tt>{@link kSTANDARDS_PORTAL_COLLECTION}</tt> collection in the users database.
+ *
+ * This operation expects the following parameters:
+ *
+ * <ul>
+ *	<li><tt>{@link kAPI_REQUEST_USER}</tt>: <em>User</em>. The identifier,
+ *		{@link kTAG_IDENTIFIER}, of the user requesting the operation, or the user
+ *		code/password combination as an array. If the user does not have the role
+ *		{@link kTYPE_ROLE_EDIT}, the service will return an error.
+ *	<li><tt>{@link kAPI_PARAM_ID}</tt>: <em>Data identifier ID</em>. This required parameter
+ *		holds the data unique identifier, {@link kTAG_NID}.
+ * </ul>
+ *
+ * The service will return the matched data or an empty array if not found.
+ */
+define( "kAPI_OP_GET_DATA",						'getData' );
+
+/**
+ * Del data.
+ *
+ * This tag defines the delete data operation.
+ *
+ * This service will delete the data identified by the {@link kAPI_PARAM_ID} parameter
+ * from the <tt>{@link kSTANDARDS_PORTAL_COLLECTION}</tt> collection in the users database.
+ *
+ * This operation expects the following parameters:
+ *
+ * <ul>
+ *	<li><tt>{@link kAPI_REQUEST_USER}</tt>: <em>User</em>. The identifier,
+ *		{@link kTAG_IDENTIFIER}, of the user requesting the operation, or the user
+ *		code/password combination as an array. If the user does not have the role
+ *		{@link kTYPE_ROLE_EDIT}, the service will return an error.
+ *	<li><tt>{@link kAPI_PARAM_ID}</tt>: <em>Data identifier ID</em>. This required parameter
+ *		holds the data unique identifier, {@link kTAG_NID}.
+ * </ul>
+ *
+ * The service will return the matched data identifier or <tt>NULL</t> if not found.
+ */
+define( "kAPI_OP_DEL_DATA",						'delData' );
+
 /*=======================================================================================
  *	REQUEST PARAMETERS																	*
  *======================================================================================*/
@@ -2035,6 +2150,14 @@ define( "kAPI_PARAM_SHAPE_OFFSET",				'shape-offset' );
  * parameter is generally used by services selecting tags or tag labels.
  */
 define( "kAPI_PARAM_EXCLUDED_TAGS",				'exclude-tags' );
+
+/**
+ * File path.
+ *
+ * This parameter represents a fie path, it is generally used to provide the file path of a
+ * data template.
+ */
+define( "kAPI_PARAM_FILE_PATH",					'path' );
 
 /**
  * Full-text search tag (string).
